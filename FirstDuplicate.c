bdc6c96ff5f1487ea19478b64625a392b34f52a0
FRANCIS CASIMIR S <51310742+FRANCIS-CASIMIR@users.noreply.github.com>
2020-05-15 10:17:03 +0530

    Update FirstDuplicate.c

--- FirstDuplicate.c
+++ FirstDuplicate.c
@@ -1,21 +1,3 @@
-#include "stdio.h"
-
-int firstDuplicate(int[],int);
-int main(int argc, char const *argv[]) {
-
-  int x[5],length=5;
-
-  x[0] = 1;
-  x[1] = 3;
-  x[2] = 5;
-  x[3] = 5;
-  x[4] = 3;
-
-  printf("%d \n",firstDuplicate(x,length));
-  return 0;
-}
-
-
 /*
 
 Returns the first repeating number .If no repeating number exists, returns -1;
