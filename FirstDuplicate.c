20ccd1d5eacc92ba83a923412ca5fb282b12f98c
FRANCIS CASIMIR S <51310742+FRANCIS-CASIMIR@users.noreply.github.com>
2020-05-15 11:56:54 +0530

    Problem statement is added

--- FirstDuplicate.c
+++ FirstDuplicate.c
@@ -1,4 +1,24 @@
 /*
+===============================================================================================================================
+------------------
+problem statement:
+------------------
+    Given an array of integers 'a' of length 'n',find out the first repeating integer. Return -1 if no duplicate is found.
+
+------------
+Constraints:
+------------
+  The values of 'a' should be between 1 and n.
+  
+  
+------------
+Examples:
+------------
+
+1)if a = {1,3,4,3,2,1} the output is 3
+2)if a = {1,2,3,4} the output is -1
+===============================================================================================================================
+
 
 Returns the first repeating number .If no repeating number exists, returns -1;
 it is a brute force version; Time complexity O(n*n);
