be66705bbbc9a1d1c588c4d54ea8236e8432e3c5
FRANCIS CASIMIR S <51310742+FRANCIS-CASIMIR@users.noreply.github.com>
2020-12-28 12:38:45 +0530

    Add files via upload

---
+++ ReArrangeArray.java
@@ -0,0 +1,42 @@
+package pkg;
+
+public class ReArrangeArray {
+	
+	/*
+	 * Given a sorted array of positive integers. 
+	 * Your task is to rearrange  the array elements alternatively 
+	 * i.e first element should be max value, second should be min value, third should be second max,
+	 *  fourth should be second min and so on.
+	 * */
+
+	int[] a,aux;
+	int len;
+	
+	ReArrangeArray(int[] a,int len){
+		this.a = a;
+		this.aux = new int[len];
+		this.len = len;
+	}
+	
+	void rearrange(){
+		
+		int max = len-1,min = 0,i=0;
+		
+		while( min <= (max/2) ){
+			System.out.println("copying "+max+" to "+i);
+			aux[i++]=a[max--];
+			System.out.println("copying "+min+" to "+i);
+			aux[i++]=a[min++];
+			
+		}
+	}
+	void printarr(){
+		
+		for(int i=0;i<len;i++){
+			System.out.print(aux[i]+" ");
+		}
+		
+		System.out.println();
+	}
+	
+}
